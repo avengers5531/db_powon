3ab1db2f0da97c33f6dd1357fedcf5f77d324232
xmithd <link@Kirino.wireless.concordia.ca>
2016-06-12 16:46:06 -0400

    Added the scripts

---
+++ scripts/create_tables.sql
@@ -0,0 +1,162 @@
+CREATE TABLE IF NOT EXISTS `region` (
+  `region_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `country`   VARCHAR(32) NOT NULL,
+  `province`  VARCHAR(32),
+  `city`      VARCHAR(32),
+  PRIMARY KEY (`region_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `member` (
+  `member_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `username`  VARCHAR(32) NOT NULL,
+  `password`  VARCHAR(255) NOT NULL,
+  `first_name` VARCHAR(64),
+  `last_name` VARCHAR(64),
+  `user_email` VARCHAR(255) NOT NULL,
+  `date_of_birth` DATE NOT NULL,
+  `registration_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_admin` CHAR(1) NOT NULL,
+  `status` CHAR(1) NOT NULL,
+  `region_access` INTEGER NOT NULL DEFAULT 0,
+  `lives_in` INTEGER DEFAULT NULL,
+  `professions_access` INTEGER NOT NULL DEFAULT 0,
+  `interests_access` INTEGER NOT NULL DEFAULT 0,
+  `dob_access` INTEGER NOT NULL DEFAULT 0,
+  `email_access` INTEGER NOT NULL DEFAULT 0,
+  `profile_picture` VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (`member_id`),
+  UNIQUE INDEX (`username`),
+  UNIQUE (`user_email`),
+  FOREIGN KEY (`lives_in`) REFERENCES `region`(`region_id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `related_members` (
+  `member_from` INTEGER NOT NULL,
+  `member_to` INTEGER NOT NULL,
+  `relation_type` CHAR(1) NOT NULL DEFAULT 'F', -- F for friends
+  `request_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `approval_date` DATETIME,
+  PRIMARY KEY (`member_from`, `member_to`),
+  FOREIGN KEY (`member_from`) REFERENCES `member`(`member_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`member_to`) REFERENCES `member`(`member_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `profession` (
+  `profession_name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`profession_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `works_as` (
+  `member_id` INTEGER NOT NULL,
+  `profession_name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY(`member_id`, `profession_name`),
+  FOREIGN KEY (`member_id`) REFERENCES `member`(`member_id`) ON DELETE CASCADE,
+  FOREIGN KEY(`profession_name`) REFERENCES `profession`(`profession_name`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `interests` (
+  `interest_name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY(`interest_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `has_interests` (
+  `interest_name` VARCHAR(255) NOT NULL,
+  `member_id` INTEGER NOT NULL,
+  PRIMARY KEY (`interest_name`, `member_id`),
+  FOREIGN KEY (`interest_name`) REFERENCES `interests`(`interest_name`) ON DELETE CASCADE,
+  FOREIGN KEY (`member_id`) REFERENCES `member`(`member_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `message_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `message_timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `subject` VARCHAR(255),
+  `body` TEXT,
+  `from_member` INTEGER,
+  PRIMARY KEY (`message_id`),
+  FOREIGN KEY (`from_member`) REFERENCES `member` (`member_id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `messages_to` (
+  `message_id` INTEGER NOT NULL,
+  `member_id` INTEGER NOT NULL,
+  `message_seen` CHAR(1) NOT NULL DEFAULT 'N',
+  `message_deleted` CHAR(1) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`message_id`, `member_id`),
+  FOREIGN KEY (`message_id`) REFERENCES `messages`(`message_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`member_id`) REFERENCES `member`(`member_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `payment_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `payment_amount` REAL NOT NULL,
+  `payment_deadline` DATETIME NOT NULL,
+  `payment_date` DATETIME,
+  `billing_period_start` DATETIME NOT NULL,
+  `billing_period_end` DATETIME NOT NULL,
+  `account_holder` INTEGER NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  FOREIGN KEY (`account_holder`) REFERENCES `member`(`member_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `powon_group` (
+  `powon_group_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `group_title` VARCHAR(64),
+  `description` TEXT,
+  `date_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `group_picture` VARCHAR(255),
+  `group_owner` INTEGER,
+  PRIMARY KEY (`powon_group_id`),
+  FOREIGN KEY (`group_owner`) REFERENCES `member`(`member_id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `is_group_member` (
+  `powon_group_id` INTEGER NOT NULL,
+  `member_id` INTEGER NOT NULL,
+  `request_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `approval_date` DATETIME,
+  PRIMARY KEY (`powon_group_id`, `member_id`),
+  FOREIGN KEY (`powon_group_id`) REFERENCES `powon_group`(`powon_group_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`member_id`) REFERENCES `member`(`member_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `page` (
+  `page_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `page_title` VARCHAR(64),
+  `date_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `page_access` CHAR(1) NOT NULL DEFAULT 'P', -- 'E' Everyone, 'P' private (only members)
+  `page_owner` INTEGER,
+  `page_group` INTEGER NOT NULL,
+  PRIMARY KEY (`page_id`),
+  FOREIGN KEY (`page_owner`) REFERENCES `member`(`member_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`page_group`) REFERENCES `powon_group`(`powon_group_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `member_can_access_page` (
+  `page_id` INTEGER NOT NULL,
+  `powon_group_id` INTEGER NOT NULL,
+  `member_id` INTEGER NOT NULL,
+  PRIMARY KEY (`page_id`, `powon_group_id`, `member_id`),
+  FOREIGN KEY (`powon_group_id`, `member_id`) REFERENCES `is_group_member`(`powon_group_id`, `member_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`page_id`) REFERENCES `page`(`page_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `post_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `post_date_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_type` CHAR(1) NOT NULL DEFAULT 'T', -- Text, Picture, Video
+  `path_to_resource` VARCHAR(255),
+  `post_body` TEXT,
+  `comment_permission` CHAR(1) NOT NULL DEFAULT 'E', -- TODO review this
+  `parent_post` INTEGER, -- it's a comment if not null
+  `page_id` INTEGER NOT NULL,
+  `author_id` INTEGER NOT NULL,
+  PRIMARY KEY (`post_id`),
+  FOREIGN KEY (`parent_post`) REFERENCES `post`(`post_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`page_id`) REFERENCES `page`(`page_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`author_id`) REFERENCES `member`(`member_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
---
+++ scripts/populate_tables.sql
@@ -0,0 +1,169 @@
+INSERT INTO region(region_id, country, province, city) VALUES (1, 'Canada', 'Quebec', 'Montreal');
+INSERT INTO region(region_id, country, province, city) VALUES (2, 'Canada', 'Ontario', 'Toronto');
+INSERT INTO region(region_id, country, province, city) VALUES (3, 'Canada', 'Quebec', 'Laval');
+INSERT INTO region(region_id, country, province, city) VALUES (4, '日本', '関東', '東京');
+INSERT INTO interests(interest_name) VALUES ('Fishing');
+INSERT INTO interests(interest_name) VALUES ('Soccer');
+INSERT INTO interests(interest_name) VALUES ('Basketball');
+INSERT INTO INTERESTS(interest_name) VALUES ('Aliens');
+INSERT INTO INTERESTS(interest_name) VALUES ('Fantasy Books');
+INSERT INTO profession(profession_name) VALUES ('Software Developer');
+INSERT INTO profession(profession_name) VALUES ('Student');
+INSERT INTO member(member_id, username, password,
+                   first_name, last_name,
+                   user_email, date_of_birth, is_admin,
+                   status, region_access, lives_in, professions_access,
+                   interests_access,
+                   dob_access, email_access,
+                   profile_picture)
+VALUES (1, 'johnsmith',
+  '$2y$10$bWyUqHoKg2USEITqwBjAyOSdrcZRve609wsIL27EumfKtGqCEMuOC',
+  'John',
+  'Smith',
+  'johnsmith@warmup.project.ca',
+  '1990-06-12', 'N', 'A', -1,3, -1, -1, -1, -1, NULL);
+
+INSERT INTO member(member_id, username, password,
+                   first_name, last_name,
+                   user_email, date_of_birth, is_admin,
+                   status, region_access, lives_in, professions_access,
+                   interests_access,
+                   dob_access, email_access,
+                   profile_picture)
+VALUES (2, 'ndalo',
+           '$2y$10$WLHWP7PM/Y6ozx6dKu7Nt.VzCsFTGGmajFUN8dXCODpFA6Ra8qixy',
+           'Ndalo',
+           'Zolani',
+           'ndalo.zolani@warmup.project.ca',
+           '1989-12-13', 'N', 'A', -1,3, -1, -1, -1, -1, NULL);
+
+INSERT INTO member(member_id, username, password,
+                   first_name, last_name,
+                   user_email, date_of_birth, is_admin,
+                   status, region_access, lives_in, professions_access,
+                   interests_access,
+                   dob_access, email_access,
+                   profile_picture)
+VALUES (2, 'ndalo',
+           '$2y$10$WLHWP7PM/Y6ozx6dKu7Nt.VzCsFTGGmajFUN8dXCODpFA6Ra8qixy',
+           'Ndalo',
+           'Zolani',
+           'ndalo.zolani@warmup.project.ca',
+           '1989-12-13', 'N', 'A', -1,3, -1, -1, -1, -1, NULL);
+
+INSERT INTO member(member_id, username, password,
+first_name, last_name,
+user_email, date_of_birth, is_admin,
+status, region_access, lives_in, professions_access,
+interests_access,
+dob_access, email_access,
+profile_picture)
+VALUES (3, 'haruhisuzumiya',
+'$2y$10$ouv/rRd9n0W7SZ2XpUL4O.FhWlfqCOQPXsY7Ni5IZvxYi/TAmqnJi',
+'ハルヒ', '涼宮',
+'suzumiya.haruhi@warmup.project.ca',
+'1992-07-26', 'Y', 'A', -1,4, -1, -1, -1, -1, NULL);
+
+INSERT INTO member(member_id, username, password,
+                   first_name, last_name,
+                   user_email, date_of_birth, is_admin,
+                   status, region_access, lives_in, professions_access,
+                   interests_access,
+                   dob_access, email_access,
+                   profile_picture)
+VALUES (4, 'robertom',
+           '$2y$10$WLHWP7PM/Y6ozx6dKu7Nt.VzCsFTGGmajFUN8dXCODpFA6Ra8qixy',
+           'Roberto',
+           'McDonald',
+           'roberto.m@warmup.project.ca',
+           '1959-04-08', 'N', 'I', -1,1, -1, -1, -1, -1, NULL);
+
+INSERT INTO member(member_id, username, password,
+                   first_name, last_name,
+                   user_email, date_of_birth, is_admin,
+                   status, region_access, lives_in, professions_access,
+                   interests_access,
+                   dob_access, email_access,
+                   profile_picture)
+VALUES (5, 'rohit',
+           '$2y$10$WLHWP7PM/Y6ozx6dKu7Nt.VzCsFTGGmajFUN8dXCODpFA6Ra8qixy',
+           'Rohit',
+           'Singh',
+           'rohit.singh@warmup.project.ca',
+           '1994-10-17', 'N', 'S', -1,2, -1, -1, -1, -1, NULL);
+
+INSERT INTO works_as(member_id, profession_name) VALUES (3, 'Student');
+INSERT INTO works_as(member_id, profession_name) VALUES (4, 'Software Developer');
+INSERT INTO has_interests(interest_name, member_id) VALUES ('Fishing', 1);
+INSERT INTO has_interests(interest_name, member_id) VALUES ('Fishing', 2);
+INSERT INTO has_interests(interest_name, member_id) VALUES ('Basketball', 2);
+INSERT INTO has_interests(interest_name, member_id) VALUES ('Aliens', 3);
+INSERT INTO has_interests(interest_name, member_id) VALUES ('Fantasy Books', 4);
+
+-- Rohit has not selected his interests yet.
+
+INSERT INTO related_members(member_from, member_to, relation_type, approval_date)
+    VALUES
+      (1, 3, 'F', CURRENT_TIMESTAMP);
+
+INSERT INTO powon_group(powon_group_id, group_title, description, group_owner)
+VALUES
+  (1, 'Lord of the Rings Fans', 'A relaxed group to share information about The Lord Of The Rings.', 3);
+
+INSERT INTO powon_group(powon_group_id, group_title, description, group_owner)
+VALUES
+  (2, 'Project R', 'A mysterious group working on the so-called ''Project R''', 4);
+
+INSERT INTO `is_group_member`(powon_group_id, member_id, approval_date) VALUES (1, 4, CURRENT_TIMESTAMP);
+INSERT INTO `is_group_member`(powon_group_id, member_id, approval_date) VALUES (2, 4, CURRENT_TIMESTAMP);
+INSERT INTO `is_group_member`(powon_group_id, member_id, approval_date) VALUES (2, 5, CURRENT_TIMESTAMP);
+
+-- Feel free to add some pages, posts, payments, messages and more relations!
+
+-- Exercise queries: they're commented
+/*
+-- 1 Show all of your tables
+show tables;
+
+-- detail on each table:
+-- desc <TABLE_NAME>;
+
+-- 2 List of all members with DOB between 1990 and 2011
+SELECT member_id, username, first_name, last_name, date_of_birth FROM member WHERE date_of_birth BETWEEN '1990-01-01' AND '2011-12-31';
+
+-- 3 Provide details of member "Roberto McDonald". Details should include personal info and groups he belongs to.
+SELECT m.member_id,
+  m.username,
+  m.first_name,
+  m.last_name,
+  m.user_email,
+  m.date_of_birth,
+  m.registration_date,
+  m.is_admin,
+  m.status,
+  r.country,
+  r.province,
+  r.city,
+  hi.interest_name,
+  wa.profession_name,
+  g.group_title
+FROM member m
+  LEFT JOIN works_as wa ON wa.member_id = m.member_id
+  LEFT JOIN has_interests hi ON hi.member_id = m.member_id
+  LEFT JOIN region r ON r.region_id = m.lives_in
+  LEFT JOIN is_group_member igm ON igm.member_id = m.member_id
+  LEFT JOIN powon_group g ON igm.powon_group_id = g.powon_group_id
+  WHERE m.first_name = 'Roberto' AND m.last_name = 'McDonald';
+
+-- 4 List of all inactive members
+SELECT member_id, username, first_name, last_name, status FROM member WHERE status = 'I';
+
+-- 5 Give a report of members who are interested in fishing and live in Laval
+SELECT m.first_name, m.last_name FROM member m
+  INNER JOIN has_interests h ON h.member_id = m.member_id AND h.interest_name = 'fishing'
+  INNER JOIN region r ON r.region_id = m.lives_in AND r.city = 'Laval';
+
+*/
+
+
+
