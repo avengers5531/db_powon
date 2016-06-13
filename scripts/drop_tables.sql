708dbc0dd7ed29b41694ea2f1d00f061bae5cea5
xmithd <link@Kirino.local>
2016-06-13 01:35:17 -0400

    Added the script to drop the tables.

---
+++ scripts/drop_tables.sql
@@ -0,0 +1,5 @@
+drop table gift_exchange, votes_on, event_details, `event`,
+post, member_can_access_page, profile_page,
+group_page, page, is_group_member, powon_group, invoice,
+messages_to, messages, has_interests, interests,
+works_as, profession, related_members, member, region;
