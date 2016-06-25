7486a3294ca8cf2c9fb677624f92be167a13e770
xmithd <link@Kirino.local>
2016-06-25 00:04:14 -0400

    Setting up the unit tests for the project

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # db_powon
-POWON project repository
+POWON project repository.
+
+## Development Setup
+Clone this project with git. Composer is required to donwload the dependencies.
+Once in the project directory, run
+```
+$ composer install
+```
+
+To prepare the database, launch the scripts in the scripts folder for your development MySQL database.
+Run `create_tables.sql` first, then optionally `populate_tables.sql` if you want some data in it.
+
+
+## Tests
+
+To run the tests with phpunit (phpunit must be installed), simply use the test_autoload.php bootstrap file.
+From the project folder run:
+```
+$ phpunit --bootstrap tests/test_autoload.php tests/
+```
--- tests/unit/MemberServiceImplTest.php
+++ tests/unit/MemberServiceImplTest.php
@@ -1,73 +1,48 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Powon\Dao\MemberDAO;
-use Powon\Entity\Member;
-
-
-class MemberDaoStub implements MemberDAO {
-    
-    private $members = array(
-        [
-            'member_id' => 1,
-            'username' => 'User1',
-            'first_name' => 'First',
-            'last_name' => 'Last',
-            'user_email' => 'test_user1@mail.ca',
-            'date_of_birth' => '1989-12-13'
-        ],
-        [
-            'member_id' => 2,
-            'username' => 'User2',
-            'first_name' => 'First2',
-            'last_name' => 'Last2',
-            'user_email' => 'test_user2@mail.ca',
-            'date_of_birth' => '1994-02-11'
-        ]);
-
-    public function getAllMembers()
-    {
-        $results = $this->members; 
-        return array_map(function($data) {
-            return new Member($data);
-        }, $results);
-    }
-
-    /**
-     * @param int $id
-     * @return Member|null
-     */
-    public function getMemberById($id)
-    {
-        // TODO: Implement getMemberById() method.
-        return null;
-    }
-
-    /**
-     * @param string $username
-     * @param bool $withPwd Set to true if you want the hashed password of the user.
-     * @return Member|null
-     */
-    public function getMemberByUsername($username, $withPwd = false)
-    {
-        // TODO: Implement getMemberByUsername() method.
-        return null;
-    }
-
-    /**
-     * @param string $email
-     * @param bool $withPwd set to true if you want the hashed password.
-     * @return Member|null
-     */
-    public function getMemberByEmail($email, $withPwd = false)
-    {
-        // TODO: Implement getMemberByEmail() method.
-        return null;
-    }
-}
+use Powon\Test\Stub\LoggerStub;
+use Powon\Test\Stub\MemberDaoStub;
 
 
 class MemberServiceImplTest extends TestCase 
 {
-    // WIP TODO
+
+    /**
+     * @var \Powon\Services\MemberService $memberService
+     */
+    private $memberService;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $dao = new MemberDaoStub();
+        $dao->members = array(
+            [
+                'member_id' => 1,
+                'username' => 'User1',
+                'first_name' => 'First',
+                'last_name' => 'Last',
+                'user_email' => 'test_user1@mail.ca',
+                'date_of_birth' => '1989-12-13'
+            ],
+            [
+                'member_id' => 2,
+                'username' => 'User2',
+                'first_name' => 'First2',
+                'last_name' => 'Last2',
+                'user_email' => 'test_user2@mail.ca',
+                'date_of_birth' => '1994-02-11'
+            ]);
+        $logger = new LoggerStub();
+        $this->memberService = new \Powon\Services\Implementation\MemberServiceImpl($logger,$dao);
+    }
+
+    public function testGetAllMembers() {
+        $res = $this->memberService->getAllMembers();
+        $this->assertEquals(count($res), 2);
+    }
+    
+    // TODO more tests
+
 }
