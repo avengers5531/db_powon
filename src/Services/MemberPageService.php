c4c7d1446d27783238d66c8194d68dc6b6fd3a08
dkundathagard <dkundathagard@gmail.com>
2016-07-10 12:10:11 -0400

    Implement data access and tests for Member Page data.

---
+++ src/Services/MemberPageService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Powon\Services;
+
+use Powon\Entity\MemberPage;
+
+interface MemberPageService {
+  /**
+   * @param int id
+   * @return a MemberPage Entity
+   */
+   public function getMemberPageByPageId($id);
+}
