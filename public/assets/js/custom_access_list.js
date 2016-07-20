587d69c80ea69bf865b1b0bb480bde390d00ef2f
xmithd <xmithd@gmail.com>
2016-07-20 16:12:32 -0400

    [pre-draft] starting work on interactive frontend stuff...

---
+++ public/assets/js/custom_access_list.js
@@ -0,0 +1,12 @@
+$(function() {
+   var list = [];
+   function appendElement(name, permission) {
+       var item = {
+           name: name,
+           permission: permission
+       };
+       list.push(item);
+   }
+
+
+});
