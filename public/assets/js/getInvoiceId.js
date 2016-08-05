f70f54b20fdae6a41926d313d0128ebb9a0c375a
dshort <dana.eliza.short@gmail.com>
2016-08-05 18:39:31 -0400

    UI fixes

--- public/assets/js/getInvoiceId.js
+++ public/assets/js/getInvoiceId.js
@@ -1,13 +1,9 @@
 jQuery(function($) {
     $('#confirmPaymentModal').on('show.bs.modal', function (e) {
         $('#confirmPaymentModal').unbind();
-        //var invoiceId = $(e.relatedTarget).data('id');
         var invoiceId = $(e.relatedTarget).attr('data-id');
-        console.log(invoiceId);
         var formAction = $('#search').attr('action');
         $('#search').attr('action', formAction + invoiceId);
         document.getElementById('invoice_id').value = invoiceId;
-
-
     });
 });
