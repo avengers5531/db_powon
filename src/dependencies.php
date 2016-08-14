ee0617775713b124953ccaaecfd6964b9e4fe253
xmithd <fireficus@gmail.com>
2016-08-14 12:13:29 -0400

    Cleaning up before project submission

--- src/dependencies.php
+++ src/dependencies.php
@@ -16,12 +16,6 @@ use \Powon\Services\Implementation\MessageServiceImpl;
 
 $container = $app->getContainer();
 
-// view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
-};
-
 $container['view'] = function ($c) {
     $settings = $c['settings']['renderer'];
     $view = new \Slim\Views\Twig($settings['template_path'], [
